9bf69d2693f0f9ccb0c9f4afe5a5eeb0a9a6500b
Musbahniar-MNR <ruang.9campus@gmail.com>
2020-11-27 15:41:56 +0700

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Form from './form';
+
+class App extends Component {
+  // AWAL
+  state = {
+    account: { username:"", password:""}
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log('Submitted');
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+
+    this.setState({ account });
+  };
+
+
+
+  // state = {
+  //   data: { email:"", password:""}
+  // }
+
+  // doSubmit = () => {
+  //   // Call the server
+  //   console.log("Submitted");
+  // };
+
+
+
+  render() {
+    const { account } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Username</label>
+            <input 
+              value={account.username}
+              onChange={this.handleChange}
+              type="text" 
+              className="form-control" 
+              id="username"
+              name="username"
+              placeholder="UserName" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              value={account.password}
+              onChange={this.handleChange} 
+              type="password" 
+              className="form-control" 
+              id="password" 
+              name="password" 
+              placeholder="Enter Password" />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+
+
+          {/* Reusable */}
+          {/* {this.renderInput("email", "Email")} */}
+          {/* {this.renderInput("password", "Password", "password")} */}
+          {/* {this.renderButton("Login")} */}
+    
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
